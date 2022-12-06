f2133d7f541c8d882568eaf64946923abc14f4bf
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-12-06 22:16:18 +0100

    init commit

---
+++ Source/MainMenuPatch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Ribworld;
+
+[StaticConstructorOnStartup]
+[HarmonyPatch]
+public static class MainMenuPatch
+{
+    
+    private static readonly Texture2D ribworld = ContentFinder<Texture2D>.Get("UI/HeroArt/Ribworld");
+    static IEnumerable<MethodBase> TargetMethods()
+    {
+        yield return AccessTools.Method(typeof(MainMenuDrawer),"MainMenuOnGUI");
+    }
+
+
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var codes = new List<CodeInstruction>(instructions);
+
+        foreach (var code in codes)
+        {
+            
+            if (code.opcode == OpCodes.Ldsfld && (FieldInfo)code.operand == AccessTools.Field(typeof(MainMenuDrawer), "TexTitle"))
+            {
+                code.operand = AccessTools.Field(typeof(MainMenuPatch), "ribworld");
+            }
+
+            yield return code;
+        }
+    }
+}
+
---
+++ Source/Ribworld.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace Ribworld;
+
+[StaticConstructorOnStartup]
+public class RibworldPatches
+{
+    static RibworldPatches()
+    {
+        Log.Warning("patching");
+        var harmony = new Harmony("com.arquebus.rimworld.mod.ribworld");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
